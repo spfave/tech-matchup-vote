c8f2e3620f1d9db015c70bae0324e105c38f6e26
Sebastian F <sebpfave2@gmail.com>
2021-07-08 22:24:36 -0400

    Create 404 page redirection

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Matchup from './pages/Matchup';
 import Vote from './pages/Vote';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/matchup" component={Matchup} />
-        <Route exact path="/matchup/:id" component={Vote} />
+      <div className='flex-column justify-center align-center min-100-vh bg-primary'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/matchup' component={Matchup} />
+          <Route exact path='/matchup/:id' component={Vote} />
+          <Route path='/404' component={NotFound} />
+          <Redirect to='/404' />
+        </Switch>
       </div>
     </Router>
   );
