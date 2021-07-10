25ca830230a9461a71e4d351b4d640b253c0b00f
Sebastian F <sebpfave2@gmail.com>
2021-07-10 14:16:40 -0400

    Add graphql wrapper to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,19 +11,26 @@ import Matchup from './pages/Matchup';
 import Vote from './pages/Vote';
 import NotFound from './pages/NotFound';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <div className='flex-column justify-center align-center min-100-vh bg-primary'>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/matchup' component={Matchup} />
-          <Route exact path='/matchup/:id' component={Vote} />
-          <Route path='/404' component={NotFound} />
-          <Redirect to='/404' />
-        </Switch>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className='flex-column justify-center align-center min-100-vh bg-primary'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/matchup' component={Matchup} />
+            <Route exact path='/matchup/:id' component={Vote} />
+            <Route path='/404' component={NotFound} />
+            <Redirect to='/404' />
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
