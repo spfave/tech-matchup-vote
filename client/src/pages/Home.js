cc4f09e65b5775428002ee13feb2c1b0e76d1341
Sebastian F <sebpfave2@gmail.com>
2021-07-10 14:19:00 -0400

    Refactor home page to graphql

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,49 +1,62 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { getAllMatchups } from '../utils/api';
+import { useQuery } from '@apollo/client';
+import { QUERY_MATCHUPS } from '../utils/queries';
+// import { getAllMatchups } from '../utils/api';
 
 const Home = () => {
-  const [matchupList, setMatchupList] = useState([]);
+  // GraphQL
+  const { loading, data } = useQuery(QUERY_MATCHUPS);
+  const matchupList = data?.matchups || [];
 
-  useEffect(() => {
-    const getMatchupList = async () => {
-      try {
-        const res = await getAllMatchups();
-        if (!res.ok) {
-          throw new Error('No list of matchups');
-        }
-        const matchupList = await res.json();
-        setMatchupList(matchupList);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getMatchupList();
-  }, []);
+  // REST API
+  // const [matchupList, setMatchupList] = useState([]);
+
+  // useEffect(() => {
+  //   const getMatchupList = async () => {
+  //     try {
+  //       const res = await getAllMatchups();
+  //       if (!res.ok) {
+  //         throw new Error('No list of matchups');
+  //       }
+  //       const matchupList = await res.json();
+  //       setMatchupList(matchupList);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+  //   getMatchupList();
+  // }, []);
 
   return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
+    <div className='card bg-white card-rounded w-50'>
+      <div className='card-header bg-dark text-center'>
         <h1>Welcome to Tech Matchup!</h1>
       </div>
-      <div className="card-body m-5">
+
+      <div className='card-body m-5'>
         <h2>Here is a list of matchups you can vote on:</h2>
-        <ul className="square">
-          {matchupList.map((matchup) => {
-            return (
-              <li key={matchup._id}>
-                <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                  {matchup.tech1} vs. {matchup.tech2}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ul className='square'>
+            {matchupList.map((matchup) => {
+              return (
+                <li key={matchup._id}>
+                  <Link to={{ pathname: `/matchup/${matchup._id}` }}>
+                    {matchup.tech1} vs. {matchup.tech2}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
-      <div className="card-footer text-center m-3">
+
+      <div className='card-footer text-center m-3'>
         <h2>Ready to create a new matchup?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
+        <Link to='/matchup'>
+          <button className='btn btn-lg btn-danger'>Create Matchup!</button>
         </Link>
       </div>
     </div>
